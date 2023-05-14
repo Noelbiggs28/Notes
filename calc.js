cd42724b05149fcc6762df2e5bbb8b8a6b782394
noel_biggs <noelbiggs28@gmail.com>
2023-05-13 17:11:41 -1000

    first commit

---
+++ calc.js
@@ -0,0 +1,20 @@
+let arr = []
+let arrend = []
+let str = ''
+let total = 0
+//enter first num
+arr.push(1)
+arr.push(2)
+//press + sign
+arrend.push(arr.join(''))
+arrend.push('+')
+arr=[]
+//enter second number
+arr.push(2)
+arr.push(1)
+//press =
+arrend.push(arr.join(''))
+total = eval(arrend.join(''))
+
+console.log(arr, arrend, total)
+console.log(arrend.slice(-1))
