cc2b7acb08b9b23f2a1cb8b87d569142e3ee0e24
noel_biggs <noelbiggs28@gmail.com>
2023-05-15 16:00:15 -1000

    apis

--- sample_flask/app.py
+++ sample_flask/app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    name = request.args.get("name", "world")
-    return render_template("index.html", name=name)
+    if request.method === "POST":
+        name = request.args.get("name", "world")
+        return render_template("index.html", name=name)
